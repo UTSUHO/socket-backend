8777111957d9e6442bcd8a2ac6a28f841c53739b
chenfengyi <chenfengyi@ictnj.ac.cn>
2022-11-14 17:06:38 +0800

    init

---
+++ server.js
@@ -0,0 +1,44 @@
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+
+const app = express();
+const httpServer = createServer(app);
+const path = "/socket/";
+const io = new Server(httpServer, {
+  /* options */
+});
+const port = 3000;
+const hostname = "127.0.0.1";
+
+io.on("connection", (socket) => {
+  connectHandler(socket);
+  console.log(socket.rooms); // Set { <socket.id> }
+  socket.join("room1");
+  console.log(socket.rooms); // Set { <socket.id>, "room1" }
+});
+
+httpServer.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+function connectHandler(socket) {
+  socket.on("error", errorHandler);
+  socket.on("close", closeHandler);
+  socket.on("message", messageHandler);
+}
+function messageHandler(e) {
+  console.info('接收客户端消息')
+  this.send(e);
+}
+
+function closeHandler(e) {
+  console.info('客户端已断开')
+}
+
+function errorHandler(e) {
+  console.info('客户端出错')
+}
+app.get("/", (req, res) => {
+  res.send("Socket IO Test Server");
+});
